736f58d849c13ef9bdf3f104a29003c1482d6727
swiftgirl1 <a.diphoorn@gmail.com>
2017-11-05 17:08:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// write your code below!
+//write your code below!
+var name = 'you'
+var holiday = 'Happy Independence Day'
+var day = 20
+
+function happyHolidays ()  {
+  console.log 'Happy holidays!'
+}
+
+
+function happyHolidaysTo(name)   {
+ console.log('happy holidays, ${name}!')
+
+ }
+
+function happyHolidayTo(holiday, name)  {
+ return '${holiday},${name}!'
+}
+
+function holidayCountdown(holiday,days)  {
+'holiday' = "Mother's Day"
+return "It's ${days} days until ${holiday}!"
+
+}
